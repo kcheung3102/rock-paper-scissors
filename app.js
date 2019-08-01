304db8520b13815b2c81a6fd0035341dced57533
kcheung3102 <kcheung3102@gmail.com>
2019-08-01 13:52:01 -0400

    intial comment

---
+++ app.js
@@ -0,0 +1,72 @@
+var userScore = 0;
+var computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_p = document.querySelector('.result > p');
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
+
+function getComputerChoice() {
+  const choices = ['rock' , 'paper', 'scissors'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function win(user, computer) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = user + 'beats ' + computer '. You Win!';
+}
+
+function lose(user, computer) {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  userScore_span.innerHTML = userScore;
+  result_div.innerHTML = computer + 'beats ' + user '. You Lose!';
+
+}
+
+function draw(user, computer) {
+
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch(userChoice + computerChoice) {
+    case 'rockscissors':
+    case 'paperrock':
+    case 'scissorspaper':
+      win(userChoice, computerChoice);
+      break;
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      lose(userChoice, computerChoice);
+      break;
+    case 'rockrock':
+    case 'paperpaper':
+    case 'scissorsscissors':
+      draw(userChoice, computerChoice);
+
+  }
+}
+
+
+function main(){
+  rock_div.addEventListener('click', function() {
+    game('rock');
+  })
+
+  paper_div.addEventListener('click', function() {
+    game('paper');
+  })
+
+  scissors_div.addEventListener('click', function() {
+    game('scissors');
+  })
+}
+
+main();
