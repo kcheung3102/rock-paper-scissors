79c67ad89e4de2cd9b04d39f00f3fda2548b77c5
kcheung3102 <kcheung3102@gmail.com>
2019-08-02 13:20:02 -0400

    different color glow to each function if you lose, win or its a draw

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
 
+//randomly generates the computers choice
 function getComputerChoice() {
   const choices = ['rock' , 'paper', 'scissors'];
   const randomNumber = Math.floor(Math.random() * 3);
@@ -18,20 +19,32 @@ function win(userChoice, computerChoice) {
   userScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
+  const userChoice_div = document.getElementById(userChoice);
   result_p.innerHTML = userChoice + ' beats ' + computerChoice + ' (comp) ' + '. You Win!';
+  userChoice_div.classList.add('green-glow');
+  setTimeout(function() {userChoice_div.classList.remove('green-glow') }, 300);
 }
 
 function lose(userChoice, computerChoice) {
   computerScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
+  const userChoice_div = document.getElementById(userChoice);
   result_p.innerHTML = computerChoice + ' (comp) ' + ' beats ' + userChoice + '. You Lost!';
+  userChoice_div.classList.add('red-glow');
+  setTimeout(function() {userChoice_div.classList.remove('red-glow') }, 300);
 }
+
 
 function draw(userChoice, computerChoice) {
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  result_p.innerHTML = computerChoice + ' equals ' + userChoice + ' Its a Draw!';
+  const userChoice_div = document.getElementById(userChoice);
+  result_p.innerHTML = 'You both picked ' + userChoice + '. Its a Draw!';
+  userChoice_div.classList.add('grey-glow');
+  setTimeout(function() {userChoice_div.classList.remove('grey-glow') }, 300);
+}
+
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
