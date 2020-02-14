e099b093b9f1edd20506945ffbed063555a2b41f
kcheung3102 <kcheung3102@gmail.com>
2020-02-14 01:21:51 -0500

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var userScore = 0;
-var computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('comp-score');
 const scoreBoard_div = document.querySelector('.score-board');
